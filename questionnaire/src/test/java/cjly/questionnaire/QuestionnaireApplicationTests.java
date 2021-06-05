3f6e212014e64d87c8585f175e44e401ab7872e0
1159286883@qq.com <59969604+1159286883@users.noreply.github.com>
2021-06-05 13:03:51 +0800

    Add files via upload

---
+++ questionnaire/src/test/java/cjly/questionnaire/QuestionnaireApplicationTests.java
@@ -0,0 +1,14 @@
+package cjly.questionnaire;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
